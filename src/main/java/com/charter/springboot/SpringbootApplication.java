674d463ae18d42cb440ae4db37f7abbec8bde7c7
chandanagannavarapu <chandana.gannavarapu@gmail.com>
2021-06-11 16:22:01 +0530

    Initial Commit

---
+++ src/main/java/com/charter/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.charter.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hiiiii")
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
